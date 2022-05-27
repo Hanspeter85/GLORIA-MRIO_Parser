4697828d94544880bf50463c115b1437faefd0b3
WU Cloud User <wucloud@firm-0474.wu.ac.at>
2022-05-27 18:07:39 +0200

    Added region groups (development, world regions and income groups)

--- R/0_create_labels.R
+++ R/0_create_labels.R
@@ -1,18 +1,53 @@
 # This script creates all labels, codes and other information that the IO calculation needs
 # It also in the end compiles the aggregation function
 
+## Import region concordances
+RegConco <- list("world" = read.xlsx( str_c("./input/", filename$RegConcordance), sheet = 3, colNames = TRUE),
+                 "income" = read.xlsx( str_c("./input/", filename$RegConcordance), sheet = 4, colNames = TRUE),
+                 "development" = read.xlsx( str_c("./input/", filename$RegConcordance), sheet = 5, colNames = TRUE) )
+
 ## Read unique lists and create labels
 unique <- list("region" = read.xlsx( str_c("./input/", filename$labels), sheet = 1, colNames = TRUE ),
                "sector" = read.xlsx( str_c("./input/", filename$labels), sheet = 2, colNames = TRUE ),
                "finaldemand" = read.xlsx( str_c("./input/", filename$labels), sheet = 3, colNames = TRUE  ),
                "extension" = read.xlsx( str_c("./input/", filename$labels), sheet = 5, colNames = TRUE  ) )
 
-# Read dimensions
+## Read region groupings and add to unique list
+
+tmp <- melt(RegConco$world, id.vars = "region_index") %>% 
+  filter(value == 1) %>% 
+  select(region_index, variable ) %>% 
+  arrange(region_index)
+
+unique$region["World_region"] <- as.character( tmp$variable )
+
+
+tmp <- melt(RegConco$income, id.vars = "region_index") %>% 
+  filter(value == 1) %>% 
+  select(region_index, variable ) %>% 
+  arrange(region_index)
+
+unique$region["Income_group"] <- as.character( tmp$variable )
+
+
+tmp <- melt(RegConco$development, id.vars = "region_index") %>% 
+  filter(value == 1) %>% 
+  select(region_index, variable ) %>% 
+  arrange(region_index)
+
+unique$region["Development_group"] <- as.character( tmp$variable )
+
+# Remove region indices in concordances
+RegConco$world$region_index <- NULL
+RegConco$income$region_index <- NULL
+RegConco$development$region_index <- NULL
+
+## Read dimensions
 nreg <- nrow(unique$region)
 nsec <- nrow(unique$sector)
 nfd <- nrow(unique$finaldemand)
 
-# Create labels for raw and parsed tables
+## Create labels for raw and parsed tables
 tmp_raw <- data.frame("index" = 1:(nreg * nsec * 2),
                       "region_code" = rep(unique$region$Lfd_Nr, each = nsec * 2),
                       "region_name" = rep(unique$region$Region_names, each = nsec * 2),
@@ -41,13 +76,14 @@ indices <- list("ind" = labels$T %>% filter(entity_code == 1) %>% pull(index),
                 "pro" = labels$T %>% filter(entity_code == 2) %>% pull(index) )
 
 # Remove redundant objects 
-remove(tmp_parsed_Y, tmp_parsed_Z, tmp_raw)
+remove(tmp_parsed_Y, tmp_parsed_Z, tmp_raw, tmp)
 
 # Write labels to folder
 fwrite( labels$parsed$Z, str_c(path$storeMRIOModel,"SectorLabels.csv") )
 fwrite( labels$parsed$Y, str_c(path$storeMRIOModel,"FinalDemandLabels.csv") )
 
-# Compile agrgegation function
+
+# Compile aggregation function
 Agg <- function(x,aggkey,dim)
 {
   if(dim == 1) x <- t(x)
